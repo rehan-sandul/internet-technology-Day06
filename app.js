e2429283c46a6425e2522db6c48ee58cc55e66aa
Rehan3807 <rehansandulsatharasinghe@gmail.com>
2026-07-04 14:39:23 +0530

    Refactor addition and subtraction functions to use Number instead of parseInt for better type handling

--- app.js
+++ app.js
@@ -110,15 +110,15 @@ function decrecounter() {
 }*/
 
 function addnumbers() {
-    let num1 = parseInt(document.getElementById("num1").value) ;
-    let num2 = parseInt(document.getElementById("num2").value) ;
+    let num1 = Number(document.getElementById("num1").value) ;
+    let num2 = Number(document.getElementById("num2").value) ;
     let result = num1 + num2;
     document.getElementById("heading").innerText = "Sum: " + result;
 }
 
 function subtractnumbers() {
-    let num1=parseInt(document.getElementById("num1").value);
-    let num2=parseInt(document.getElementById("num2").value);
+    let num1=Number(document.getElementById("num1").value);
+    let num2=Number(document.getElementById("num2").value);
     let result = num1 - num2;
     document.getElementById("heading").innerText = "Difference: " + result;
 }
